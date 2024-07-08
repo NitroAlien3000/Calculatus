f7ef2ebbb01f06cba9e4d00995b3294b01794be3
NitroAlien3000 <159768233+NitroAlien3000@users.noreply.github.com>
2024-07-08 13:33:35 +0600

    Update contact_validate.js

--- contact_validate.js
+++ contact_validate.js
@@ -6,7 +6,6 @@ function validate() {
     var contactMethodPhone = document.querySelector("#phoneContact");
     var show = document.querySelector("#show");
 
-    // Clear any previous messages
     show.innerText = "";
 
     if (name.value === "" || name.value == null) {
@@ -23,13 +22,4 @@ function validate() {
         show.innerText = "Please select a preferred contact method.";
         return false;
     }
-
-    show.innerText = "Successfully submitted.";
-    
-    // Delay form submission by 2 seconds
-    setTimeout(function() {
-        document.querySelector("form").submit();
-    }, 2000);
-
-    return false; // Prevent the default form submission to handle it via JavaScript
 }
